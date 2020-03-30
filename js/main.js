9bb4e2e9e3346737c564b5016c14b4d85541972c
JolantaHuba <huba.jolanta@gmail.com>
2020-03-30 18:29:40 +0200

    Fix menu (scroll-lock)

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const menuList = document.querySelector('.menu__list');
 function toggleMenu() {
   burgerBtn.classList.toggle('active');
   menuList.classList.toggle('active');
+  document.body.classList.toggle('scroll-lock');
   if (burgerBtn.classList.contains('active')) {
     burgerBtn.setAttribute('aria-expanded', 'true');
   } else burgerBtn.setAttribute('aria-expanded', 'false');
