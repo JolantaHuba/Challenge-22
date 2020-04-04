d562f5c29b0ec83f27e9a62756b3c7aa73d76fbc
JolantaHuba <huba.jolanta@gmail.com>
2020-04-04 18:56:45 +0200

    Conters done

--- js/main.js
+++ js/main.js
@@ -35,3 +35,53 @@ function filterItems() {
 categoryBtns.forEach((btn) => {
   btn.addEventListener('click', filterItems);
 });
+
+
+// Counters
+const countersSection = document.querySelector('.counters');
+let counterOn = true;
+
+const counters = {
+  delivered: 518,
+  webdesign: 177,
+  hours: 2465,
+  coffee: 1893,
+}
+
+const countersDisplay = {
+  delivered: document.querySelector('[data-counter="delivered"]'),
+  webdesign: document.querySelector('[data-counter="webdesign"]'),
+  hours: document.querySelector('[data-counter="hours"]'),
+  coffee: document.querySelector('[data-counter="coffee"]'),
+}
+
+function setCounter(displayItem, maxValue) {
+  let counter = 0;
+  const id = setInterval(() => {
+    if (maxValue > 1000) {
+      counter > maxValue * 0.8 ? counter++ : counter += 10;
+    } else {
+      counter++
+    };
+
+    displayItem.textContent = counter;
+    if (counter === maxValue) {
+      clearInterval(id);
+    }
+  }, 8);
+}
+
+function startCounters() {
+  console.log('startcounters');
+  setCounter(countersDisplay.delivered, counters.delivered);
+  setCounter(countersDisplay.webdesign, counters.webdesign);
+  setCounter(countersDisplay.hours, counters.hours);
+  setCounter(countersDisplay.coffee, counters.coffee);
+  counterOn = false;
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > countersSection.offsetTop - window.innerHeight && counterOn) {
+    startCounters();
+  }
+});
